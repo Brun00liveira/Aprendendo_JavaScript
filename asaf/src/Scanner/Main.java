962aa87aa94d1c53f1b3e1f5f8db80a1f892325e
Bruno Oliveira <brunobromo321@gmail.com>
2023-01-29 22:45:51 -0300

    Java

---
+++ asaf/src/Scanner/Main.java
@@ -0,0 +1,51 @@
+
+package Scanner;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+//		String x;
+//		x = sc.next();
+		
+//		int x;
+//		x = sc.nextInt();
+		
+//		double x;
+//		x = sc.nextDouble();
+//		Locale.setDefault(Locale.US);
+		
+//		char x;
+//		x = sc.next().charAt(0);
+		
+//		System.out.println("Voce digitou " + x);
+		
+//		String x;
+//		int y;
+//		double z;
+//		x = sc.next();
+//		y = sc.nextInt();
+//		z = sc.nextDouble();
+//		Locale.setDefault(Locale.US);
+//		System.out.println("Dados digitados: ");
+//		System.out.println(x);
+//		System.out.println(y);
+//		System.out.println(z);
+		
+		String s1, s2, s3;
+		
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+		
+		
+		sc.close();
+		
+	}
+}
---
+++ asaf/src/asaf/caaaa.java
@@ -0,0 +1,26 @@
+package asaf;
+
+import java.util.Locale;
+
+public class caaaa {
+
+//		public static void main(String[] args) {
+//		double x = 10.434535;
+//		// TODO Auto-generated method stub
+//		System.out.printf("%.2f\n", x);
+//		Locale.setDefault(Locale.US);
+//		System.out.printf("%.2f\n", x);
+//		System.out.printf("Resultado = %.2f metros", x);
+//	}
+
+	public static void main(String [] args) {
+		String nome = "bruno";
+		int idade = 12;
+		double peso = 108.4543;
+		
+		System.out.printf("Meu nome é" + nome + "tenho "+ idade + " e "+ peso + " kilos");
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais\n", nome, idade, peso);
+	}
+	}
+
+
